75d92151b3e96893830a0ef92bd84059b49484d9
sgr98 <60283755+sgr98@users.noreply.github.com>
2023-02-25 00:42:30 +0530

    Added css for App and LoginBar, created LoginBar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import LoginBar from './Components/LoginBar';
 import Contacts from './Components/Contacts';
 import ChatWindow from './Components/ChatWindow';
 import './App.css';
 
 const App = () => {
+    const [user, setUser] = useState('');
+    const handleLoginSubmit = async (e) => {
+        e.preventDefault();
+        console.log(user);
+
+        // const 
+    };
+
     return (
         <div className="App">
-            <LoginBar />
-            <Contacts />
-            <ChatWindow />
+            <div className="App__LoginBar">
+                <LoginBar
+                    user={user}
+                    setUser={setUser}
+                    handleLoginSubmit={handleLoginSubmit}
+                />
+            </div>
+            <div className="App__Chat">
+                <Contacts />
+                <ChatWindow />
+            </div>
         </div>
     );
 };
--- src/Components/LoginBar/index.js
+++ src/Components/LoginBar/index.js
@@ -1,12 +1,23 @@
-import React from 'react'
-import "./index.css"
+import React from 'react';
+import './index.css';
 
-const LoginBar = () => {
-  return (
-    <div className='LoginBar'>
-      LoginBar
-    </div>
-  )
-}
+const LoginBar = ({user, setUser, handleLoginSubmit}) => {
+    return (
+        <div className="LoginBar">
+            <form className="LoginBar__form" onSubmit={handleLoginSubmit}>
+                <input
+                    type="text"
+                    placeholder="Enter username"
+                    value={user}
+                    onChange={(e) => setUser(e.target.value)}
+                    className="LoginBar__form--input"
+                />
+                <button type="submit" className="LoginBar__form--submit-button">
+                    SUBMIT
+                </button>
+            </form>
+        </div>
+    );
+};
 
-export default LoginBar
+export default LoginBar;
